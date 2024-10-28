c84df7c2beda2dff80c3dc038576f8f20d749d20
JustBryse <brysonmacduff@gmail.com>
2024-10-27 22:22:18 -0400

    updated gitignore

---
+++ build/release/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/bryson/projects/c++/cpp-template
+# Build directory: /home/bryson/projects/c++/cpp-template/build/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("inc")
+subdirs("tests")
---
+++ build/release/src/CMakeFiles/cpp-template.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-template.dir/main.cpp.o"
+  "CMakeFiles/cpp-template.dir/main.cpp.o.d"
+  "CMakeFiles/cpp-template.dir/shape.cpp.o"
+  "CMakeFiles/cpp-template.dir/shape.cpp.o.d"
+  "cpp-template"
+  "cpp-template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
